7fa387a3bdb1a1e74747383f9fea1a22d60206f9
Managing a Book Trading Club (6) <sasitha2005@gmail.com>
2025-02-25 13:43:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: req.socket.remoteAddress, language: req.headers['accept-language'], software: req.headers['user-agent'] });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3001, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
